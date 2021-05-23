ade972af64e8973319dfe0ceb537dfef83b9faea
Patrik Freij <patrik.t.freij@gmail.com>
2021-05-23 18:55:11 +0200

    Do some processing of the path before returning it

--- modules/location.go
+++ modules/location.go
@@ -1,5 +1,10 @@
 package modules
 
+import (
+	"os"
+	"strings"
+)
+
 type Location struct {
 	Path string
 }
@@ -8,6 +13,16 @@ func (location *Location) SetPath(path string) {
 	location.Path = path
 }
 
+func (location *Location) processPath() string {
+	outputPath := location.Path
+	userHome := "/home/" + os.Getenv("USER") + "/"
+	if strings.HasPrefix(outputPath, userHome) {
+		outputPath = strings.Replace(outputPath, userHome, "~/", 1)
+	}
+
+	return outputPath
+}
+
 func (location *Location) GetOutput() string {
-	return location.Path
+	return location.processPath()
 }
